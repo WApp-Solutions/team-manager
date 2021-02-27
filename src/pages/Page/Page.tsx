aa021cdfeff7bffc3513ee3761f7d7a1947ee2a2
Fabian Hilz <fabian.hilz@t-online.de>
2021-02-27 10:39:18 +0100

    WIP: add icon to reflect db connection status

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -1,10 +1,13 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem } from '@ionic/react';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
 import { ExploreContainer } from '../../components';
 
 import { demoActions } from '../../core/store/demo/demo.slice';
+import { DatabaseConnectionIcon } from '../../components/DatabaseConnectionIcon';
+
+import localStyles from './Page.module.scss';
 
 const Page: React.FC = () => {
   const { name } = useParams<{ name: string }>();
@@ -22,6 +25,9 @@ const Page: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle onClick={handleAdd}>{name}</IonTitle>
+          <IonItem className={localStyles.ionItem} slot="end">
+            <DatabaseConnectionIcon />
+          </IonItem>
         </IonToolbar>
       </IonHeader>
 
