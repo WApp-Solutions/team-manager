229957508f6d22190ef4327c684472ab2365ac03
Fabian Hilz <fabian.hilz@t-online.de>
2021-12-18 12:30:55 +0100

    (#19) Update all dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-
-import Page from './pages/Page/Page';
-import LoginPage from './pages/Login/LoginPage';
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -23,7 +16,16 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.scss';
+
+import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Redirect, Route } from 'react-router-dom';
+
 import { Menu } from './components';
+import LoginPage from './pages/Login/LoginPage';
+import Page from './pages/Page/Page';
+
+setupIonicReact();
 
 // User is authenticated and has access to protected views
 const isAuthenticated = false;
